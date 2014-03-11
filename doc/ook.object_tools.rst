528a8f51deb4987d5260bd44bbfd56dcc2b3d087
raulg <raulg@google.com>
2014-03-11 00:44:17 -0700

    Finish up the object_tools documentation.

--- doc/ook.object_tools.rst
+++ doc/ook.object_tools.rst
@@ -2,4 +2,16 @@ object_tools API
 ===================
 
 .. automodule:: ook.object_tools
-  :members:
+
+Method Definitions
+-------------------
+
+.. automethod:: ook.object_tools.create_ook_type
+
+.. automethod:: ook.object_tools.validate_object
+
+.. automethod:: ook.object_tools.validate_property
+
+.. automethod:: ook.object_tools.validate_schema
+
+.. automethod:: ook.object_tools.validate_value
--- ook/object_tools.py
+++ ook/object_tools.py
@@ -1,9 +1,41 @@
+"""The **object_tools** provides the methods to create and validate **Ook** types and objects.
+
+Usage
+======
+
+There are two basic operations provided by **object_tools**; schema definition and object
+handling. Schema definition entails type creation and validation.
+
+Schema Tools
+-------------
+
+To validate a schema definition, utilize the :meth:`~ook.object_tools.validate_schema` method.
+
+Schema are composed of :class:`~ook.object_type.PropertySchema` objects. If there is a need,
+individual **PropertySchema** objects can be validated individually with the
+:meth:`~ook.object_tools.validate_property` method.
+
+To create a python type for a given :class:`~ook.object_type.SchemaType` utilize the
+:meth:`~ook.object_tools.create_ook_type` method. **Ook** object instances created by a generated
+type are child classes of the :class:`~ook.object_types` class.
+
+Object Tools
+-------------
+
+**Ook** objects created by either subclassing :class:`~ook.object_type.BaseType` or via
+:meth:`~create_ook_type`, will need to be validated. Utilize the
+**Ook** object :meth:`~ook.object_tools.validate_object` method for validation.
+
+If the need should arise for validation of an **Ook** object by value, utilize the
+:meth:`~ook.object_tools.validate_value` method.
+
+"""
 import re
 
 from object_type import BaseType, PropertySchema, SchemaType
 
 #: The `type_map` converts the string declaration of attribute type.
-type_map = BaseType({
+type_map = {
     'bool': bool,
     'dict': dict,
     'float': float,
@@ -11,14 +43,14 @@ type_map = BaseType({
     'list': list,
     'set': set,
     'str': basestring,
-})
+}
 
 #: The `collection_type_set` is the set of supported collection types.
 collection_type_set = {dict, list, set}
 
 
 def create_ook_type(name, schema):
-    """
+    """Create an **Ook** type to generate objects with a given schema.
 
     :param name: The name to apply to the created class, with object_type.BaseType as parent.
     :type name: str
@@ -26,13 +58,14 @@ def create_ook_type(name, schema):
     :type schema: dict
     :return: A class whose base is object_type.BaseType.
     :rtype: ClassType
+    :except ValueError: String name required. Dict or SchemaType schema required.
     """
     if name is None or name is '':
         raise ValueError('The string "name" argument is required.')
     if schema is None:
         raise ValueError('The schema dictionary is required.')
     if not isinstance(schema, dict):
-        raise ValueError('The schema must be a dict.')
+        raise ValueError('The schema must be a dict or SchemaType.')
 
     ook_type = type(name, (BaseType, ), dict())
 
@@ -48,53 +81,17 @@ def create_ook_type(name, schema):
     return ook_type
 
 
-def validate_property(the_property):
-    """
-
-    :param the_property:
-    :type the_property:
-    :return:
-    :rtype:
-    """
-    if not isinstance(the_property, PropertySchema):
-        if isinstance(the_property, dict) or isinstance(the_property, BaseType):
-            the_property = PropertySchema(the_property)
-        else:
-            raise ValueError(
-                '"the_property" argument must be of type dict, BaseType, or PropertyType.')
-
-    validate_object(the_property)
-
-
-def validate_schema(the_schema):
-    """
-
-    :param the_schema:
-    :type the_schema: dict|ook.object_type.SchemaType
-    :return:
-    :rtype:
-    """
-    if not isinstance(the_schema, SchemaType):
-        if isinstance(the_schema, dict) or isinstance(the_schema, BaseType):
-            the_schema = SchemaType(the_schema)
-        else:
-            raise ValueError(
-                '"the_schema" argument must be of type dict, BaseType, or SchemaType,')
-
-    #todo: raul - validation should happen like this, but will defer until nested validation.
-    # validate_object(the_schema)
-
-    for property in the_schema.values():
-        validate_property(property)
-
-
 def validate_object(the_object):
     """Method that will validate if an object meets the schema requirements.
 
     :param the_object: An object instance whose type is a child class of
-        `ook.object.BaseType`
+        :class:`~ook.object_type.BaseType`
     :type the_object: ook.object_type.BaseType
-    :except: ValueError - Thrown due to validation failures.
+    :except ValueError:
+        * *the_object* is not a :class:`~ook.object_type.BaseType`.
+
+        * A property of *the_object* does not meet schema requirements.
+
     """
 
     if not isinstance(the_object, BaseType):
@@ -137,8 +134,54 @@ def validate_object(the_object):
         raise ValueError(str.join(' \n-- ', value_errors))
 
 
+def validate_property(the_property):
+    """Method to validate if a schema property object is a valid definition.
+
+    :param the_property: The object to be validated as a :class:`~ook.object_type.PropertySchema`
+        candidate.
+    :type ook.object_type.PropertySchema|dict:
+    :except ValueError: *the_property* is  not dict, :class:`~ook.object_type.BaseType`
+        or :class:`~.ook.object_type.PropertySchema`.
+    """
+    if not isinstance(the_property, PropertySchema):
+        if isinstance(the_property, dict) or isinstance(the_property, BaseType):
+            the_property = PropertySchema(the_property)
+        else:
+            raise ValueError(
+                '"the_property" argument must be of type dict, BaseType, or PropertySchema.')
+
+    validate_object(the_property)
+
+
+def validate_schema(the_schema):
+    """Determine if a schema definition is a valid :class:`~ook.object_type.SchemaType` candidate.
+
+    :param the_schema: The schema definition candidate.
+    :type the_schema: dict|ook.object_type.SchemaType
+    :except ValueError:
+
+        * *the_schema* is not a dict, **BaseType**, or *SchemaType*.
+
+        * *the_schema* is not a valid **Ook** schema definition.
+    """
+    if not isinstance(the_schema, SchemaType):
+        if isinstance(the_schema, dict) or isinstance(the_schema, BaseType):
+            the_schema = SchemaType(the_schema)
+        else:
+            raise ValueError(
+                '"the_schema" argument must be of type dict, BaseType, or SchemaType,')
+
+    #todo: raul - validation should happen like this, but will defer until nested validation.
+    # validate_object(the_schema)
+
+    for property in the_schema.values():
+        validate_property(property)
+
+
+#todo: raulg - This is going to get revamped in next rev.
 def validate_value(key, metadata, value_type, value, value_errors):
     """ Method to validate an object value meets schema requirements.
+
     :param key:
     :type key:
     :param metadata:
