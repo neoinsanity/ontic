6f54677446208266e9266157a03566d1c9a3ed32
raulg <raulg@google.com>
2014-03-11 18:28:22 -0700

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ook',
-    version='0.0.1',
+    version='0.0.2',
     license='Apache License 2.0',
     description='Qualities and Quantities are the stuff of Objects.',
     packages=['ook',],
