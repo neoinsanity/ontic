9e2c1f22d83f99429a7d97b8aba379ffe4844ba6
neoinsanity <mindbender@gmail.com>
2014-04-01 01:16:00 -0700

    Forgot to remove the object_tool file.

--- ook/__init__.py
+++ ook/__init__.py
@@ -1,7 +1,5 @@
 import meta_type
 import object_type
-import object_tool
 import schema_type
 
-__all__ = ['meta_type', 'object_tool',
-           'object_type', 'schema_type']
+__all__ = ['meta_type', 'object_type', 'schema_type']
