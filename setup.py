71d641fe30295a7f6e8fdd79412beddc9ca0bbeb
neoinsanity <mindbender@gmail.com>
2015-01-12 18:16:01 -0800

    Preped for 0.0.5 release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.txt') as file:
 
 setup(
     name='ontic',
-    version='0.0.4',
+    version='0.0.5a',
     author='Raul Gonzalez',
     author_email='mindbender@gmail.com',
     url='https://github.com/neoinsanity/ontic',
