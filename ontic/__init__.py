ff88229de164cd4c0cc2cf1d74d6c46199e73a71
neoinsanity <mindbender@gmail.com>
2016-02-19 13:24:21 -0800

    Added the ontic_core tests.

---
+++ ontic/__init__.py
@@ -0,0 +1,4 @@
+
+from ontic import ontic_core
+
+__all__ = ['ontic_core']
---
+++ test/utils.py
@@ -0,0 +1,35 @@
+"""Utilities for testing."""
+import unittest
+
+import ontic
+
+
+class BaseTestCase(unittest.TestCase):
+    """BaseTest case has methods to test Ontic features and functionality."""
+
+    def assert_dynamic_accessing(
+            self, ontic_object: 'ontic.ontic_core.OnticCore') -> None:
+        """Assert that ontic_object exhibits dynamic property accessing.
+
+        :param ontic_object: Ontic object to validate for dynamic access.
+        :type ontic_object: core_type.CoreType
+        """
+        # Assignment by attribute
+        ontic_object.attr1 = 1
+        self.assertEqual(1, ontic_object.attr1)
+        self.assertEqual(1, ontic_object['attr1'])
+        ontic_object.attr2 = 'Some string'
+        self.assertEqual('Some string', ontic_object.attr2)
+        self.assertEqual('Some string', ontic_object['attr2'])
+
+        # Assignment by key
+        ontic_object['key1'] = 3
+        self.assertEqual(3, ontic_object['key1'], 3)
+        self.assertEqual(3, ontic_object.key1)
+        ontic_object['key2'] = 'Some value'
+        self.assertEqual('Some value', ontic_object['key2'])
+        self.assertEqual('Some value', ontic_object.key2)
+
+        # Retrieval failures follow expected interface behavior
+        self.assertRaises(AttributeError, getattr, ontic_object, 'no_attribute')
+        self.assertRaises(KeyError, lambda: ontic_object['bad_key'])
